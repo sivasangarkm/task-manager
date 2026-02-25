23f2618ea16c0b106cc4b439d0badca3c057b21f
sivasangarkm <sivasangar_km@live.com>
2026-02-26 03:37:54 +0530

    7-5-Fetching data using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
-const connect = () => console.log("Connecting...");
-const disconnect = () => console.log("Disconnecting...");
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
-    connect();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
 
-    return () => {
-      disconnect();
-    };
-  });
-
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
